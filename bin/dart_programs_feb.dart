664cf6e1f469cdc326f92145494abd7e09864428
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-02-26 10:36:01 +0530

    Initial commit

---
+++ bin/dart_programs_feb.dart
@@ -0,0 +1,5 @@
+import 'package:dart_programs_feb/dart_programs_feb.dart' as dart_programs_feb;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_programs_feb.calculate()}!');
+}
---
+++ bin/sample2.dart
@@ -0,0 +1,10 @@
+void main() {
+  ///syntax => datatype variable_name = value;
+  String firstname = 'anu';
+  int age = 20;
+  double cgpa = 7.8;
+  int a = 30;
+  int b = 40;
+  print('my name is $firstname');
+  print('sum of a and b = ${a + b}');
+}
